8db7d57d4299f2dd7b9fe66ac043d19cd2dd70d2
murad-97 <muradalshorman136@gmail.com>
2023-10-17 08:40:04 +0300

    murad

--- backend/app/Http/Middleware/RedirectIfAuthenticated.php
+++ backend/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,8 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                // return response()->json(Auth::user());
+                return redirect("/user");
             }
         }
 
