463a8e29ad2a4cd9db76a7531d13f677aaa764bc
razan alkateeb <razan.alkateeb0@gmail.com>
2023-10-20 00:18:50 +0300

    razan

--- Jobcy/src/App.js
+++ Jobcy/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Routes from "./Routes/index";
-import { Provider } from 'react-redux';
-import store from './pages/ExtraPages/Components/redux/store';
-
+import { Provider } from "react-redux";
+import store from "./pages/ExtraPages/Components/redux/store";
+import { DataProvider } from "./pages/FilterData/DataContext";
 
 //import Custom Style scss
 import "./assets/scss/themes.scss";
 function App() {
   return (
-    <React.Fragment>
-    {/* Provide the Redux store to your components */}
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  </React.Fragment>
+    <DataProvider>
+      <React.Fragment>
+        {/* Provide the Redux store to your components */}
+        <Provider store={store}>
+          <Routes />
+        </Provider>
+      </React.Fragment>
+    </DataProvider>
   );
 }
 
